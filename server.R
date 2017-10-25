80177236d04471c9bd3207a0da6ddede8d451587
kkiviat <kkiviat@gmail.com>
2017-10-24 20:28:28 -0700

    created tabs for clustering, added details to job descriptions

--- server.R
+++ server.R
@@ -5,8 +5,29 @@ library(SnowballC)
 library(dplyr)
 library(readr)
 
+remove_word_from_dtm = function(dtm, w) {
+    col <- which(colnames(dtm) == w)
+    ## remove documents not containing w
+    dtm <- dtm[dtm[, w] > 0,]
+    ## remove column corresponding to w
+    dtm <- dtm[, -col]
+    ## remove columns of words not appearing in remaining documents
+    dtm <- dtm[, colSums(dtm) > 0]
+    return(dtm)
+}
+
+## given a data frame built from a DocumentTermMatrix
+## and a list of numbers specifying some of these documents,
+## creates a data frame suitable for wordcloud2
+create_wc2_input_from_documents <- function(doc_list, df) {
+    df = df[doc_list, ]
+    data.frame(word=colnames(df),
+               freq=colSums(df))
+}
+
 summaries <- data.frame(read_tsv('data/summaries.txt', col_names=FALSE))
-documents <- paste(summaries$X1, summaries$X2, sep='<br/>')
+links <- paste0('<a href=', summaries$X3, '>', summaries$X4, '</a>')
+documents <- paste(links, summaries$X5, summaries$X1, summaries$X2, sep='<br/>')
 desired_experience <-
       lapply(summaries[,2], function(l)
           do.call(paste, as.list(gsub(' ', '_', unlist(strsplit(l, ', '))))))
@@ -42,13 +63,21 @@ all_postings <- apply(all_postings, c(1,2), function(x) ifelse(x > 0, 1, 0))
 ## regexp which is messed up by including the character '+')
 colnames(all_postings) <- sapply(colnames(frequencies), function(x) ifelse(grepl('\\+', x), x, stemCompletion(x, corpus)))
 
-## ## temporary to remove NAs
-## all_postings <- all_postings[,!is.na(colnames(all_postings))]
 
 filtered_postings <- all_postings
 freq_df <- data.frame(word=colnames(all_postings), freq=colSums(all_postings))
 
-#### This prints the filter_list in the UI!!!!!
+###########################
+## Clustering
+###########################
+distMatrix <- dist(scale(frequencies))
+fit <- hclust(distMatrix, method="ward.D")
+
+
+###########################
+## Server
+###########################
+
 shinyServer(function(input, output, session) {
     ## make wordcloud2 use the same seed each time it is called
     wordcloud2_rep <- repeatable(wordcloud2)
@@ -59,13 +88,9 @@ shinyServer(function(input, output, session) {
                             all_doc_words=c())
 
     filtered_df <- reactive({
-        df <- arrange(data.frame(word=colnames(values$filtered_postings),
-                                freq=colSums(values$filtered_postings)),
-                     desc(freq))
-        ## remove the words that appear in all remaining documents
-        ##df <- filter(df, freq < nrow(values$filtered_postings))
-        print(df)
-        df
+        arrange(data.frame(word=colnames(values$filtered_postings),
+                           freq=colSums(values$filtered_postings)),
+                desc(freq))
     })
 
     ## ignoreInit so it doesn't raise an error before word() has
@@ -74,26 +99,21 @@ shinyServer(function(input, output, session) {
         w <- word()
         if (! w %in% values$filter_list) {
             values$filter_list <- c(values$filter_list, w)
-            #print(values$filter_list)
         }
         if (w %in% colnames(values$filtered_postings)) {
-            col <- which(colnames(values$filtered_postings) == w)
-            ## remove documents not containing w
             values$filtered_postings <-
-                values$filtered_postings[values$filtered_postings[,w] > 0,]
-            ## remove column corresponding to w
-            values$filtered_postings <- values$filtered_postings[,-col]
-            ## remove columns of words not appearing in remaining documents
-            values$filtered_postings <-
-                values$filtered_postings[,colSums(values$filtered_postings) > 0]
+                remove_word_from_dtm(values$filtered_postings, w)
+
+            in_all_docs <-
+                colSums(values$filtered_postings) == nrow(values$filtered_postings)
+
+            ## save words in all documents
             values$all_doc_words <- c(values$all_doc_words,
-                colnames(values$filtered_postings)[
-                    colSums(values$filtered_postings) == nrow(values$filtered_postings)
-                ])
+                                     colnames(values$filtered_postings)[in_all_docs])
+
             ## remove words in all documents
             values$filtered_postings <-
-                values$filtered_postings[,colSums(values$filtered_postings) !=
-                                          nrow(values$filtered_postings)]
+                values$filtered_postings[, !in_all_docs]
         }
     }, ignoreInit=TRUE)
 
@@ -105,8 +125,8 @@ shinyServer(function(input, output, session) {
     num_docs <- reactive({nrow(values$filtered_postings)})
     output$num_docs <- renderText(num_docs())
 
-    all_doc_words <- reactive({as.list(values$all_doc_words)})
-    output$all_doc_words <- renderText(do.call(paste, all_doc_words()))
+    all_doc_words <- reactive({values$all_doc_words})
+    output$all_doc_words <- renderText(do.call(paste, as.list(all_doc_words())))
 
     filter_words <- reactive({as.list(values$filter_list)})
     output$filter_words <- renderText(do.call(paste, filter_words()))
@@ -118,11 +138,43 @@ shinyServer(function(input, output, session) {
             wordcloud2_rep(df, rotateRatio=0)
         }
     })
-
     output$wordCloud <- renderWordcloud2({wordCloud()})
 
     observe({print(paste('word:', gsub(":.*", "", input$selected_word)))})
     word <- reactive({gsub(":.*", "", input$selected_word)})
-
     output$selected_word <- renderText(word())
+
+
+    ## cluster_list <- reactive({
+    ##     clusters <- cutree(fit, input$k)
+    ##     clusters <- split(names(clusters), clusters)
+    ##     print(clusters)
+    ##     df_list <- lapply(clusters, function(doc_list) {
+    ##         create_wc2_input_from_documents(doc_list, all_postings)
+    ##         })
+    ##     df_list
+    ## })
+
+    ## output$clusters <- renderUI({
+    ##     wordcloud2Output_list <- lapply(1:input$k, function(i) {
+    ##         wordcloud2Output(paste0("cluster", i))
+    ##     })
+    ##     print(tagList(wordcloud2Output_list))
+    ##     do.call(tagList, wordcloud2Output_list)
+    ## })
+
+  ##   ## based on https://gist.github.com/wch/5436415
+  ##   for (i in 1:max_clusters) {
+  ##   # Need local so that each item gets its own number. Without it, the value
+  ##   # of i in the renderPlot() will be the same across all instances, because
+  ##   # of when the expression is evaluated.
+  ##   local({
+  ##     my_i <- i
+  ##     cluster_name <- paste0("cluster", my_i)
+  ##     output[[cluster_name]] <- renderWordcloud2({
+  ##       wordcloud2_rep(cluster_list()[[my_i]])
+  ##     })
+  ##   })
+  ## }
+
 })
--- ui.R
+++ ui.R
@@ -15,17 +15,31 @@ shinyUI(fluidPage(
     titlePanel("Word cloud for job search results"),
     sidebarLayout(
         sidebarPanel(
-            tags$head(tags$script(HTML(jscode))),
-            h3("Number of postings:"),
-            textOutput("num_docs"),
-            h3("Filtered words:"),
-            textOutput("filter_words"),
-            h3("Words in all postings:"),
-            textOutput("all_doc_words")
+            conditionalPanel(condition="input.conditionedPanels==1",
+                             tags$head(tags$script(HTML(jscode))),
+                             h3("Number of postings:"),
+                             textOutput("num_docs"),
+                             h3("Filtered words:"),
+                             textOutput("filter_words"),
+                             h3("Words in all postings:"),
+                             textOutput("all_doc_words")
+                             ),
+            conditionalPanel(condition="input.conditionedPanels==2",
+                             sliderInput("k", "Enter number of clusters", 1, max_clusters, 2)
+                             )
         ),
         mainPanel(
-            withSpinner(wordcloud2Output("wordCloud")),
-            htmlOutput("jobs")
+            tabsetPanel(
+                tabPanel("filter", value=1,
+                         withSpinner(wordcloud2Output("wordCloud")),
+                         htmlOutput("jobs")
+                         ),
+                tabPanel("cluster", value=2,
+                         uiOutput("clusters")),
+                         ## wordcloud2Output("cluster1"),
+                         ## wordcloud2Output("cluster2")),
+                id = "conditionedPanels"
+        )
         )
     )
 ))
